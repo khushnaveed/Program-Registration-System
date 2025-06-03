8523f279db2d1b508bced028f627efa38e57e6fe
Khush Bakht Naveed <khush.naveed@dci-student.org>
2025-06-03 09:58:49 +0200

    Hero section update on aplly form

--- frontend/src/pages/ApplicationForm.jsx
+++ frontend/src/pages/ApplicationForm.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import HeroSection from '../components/HeroSection'
+import React from "react";
+import HeroSection from "../components/HeroSection";
 
 export default function ApplicationForm() {
   return (
     <>
-         <HeroSection
-                title="Contact Us"
-                subtitle="We’re here to assist you — reach out anytime"
-                backgroundImage="https://plus.unsplash.com/premium_photo-1677567996070-68fa4181775a?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              />
+      <HeroSection
+        title="Application Form"
+        subtitle="Complete the form below to apply for our programs"
+        backgroundImage="https://plus.unsplash.com/premium_photo-1677567996070-68fa4181775a?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
     </>
-  )
+  );
 }
